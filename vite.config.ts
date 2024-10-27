e07d3680759fac877a94d684aaa48c85b9feda22
polyakov-artem <polyakov.contact@yandex.ru>
2024-10-27 20:24:58 +0300

    build: add coverage folder to .gitignore, add `@vitest/ui`, add test:ui script, change vitest.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
+    exclude: [...configDefaults.exclude, 'sundae-server'],
     globals: true,
     environment: 'jsdom',
     setupFiles: ['src/__tests__/setup.ts'],
     coverage: {
-      provider: 'v8', // or 'v8'
+      provider: 'v8',
+      reportOnFailure: true,
     },
   },
 });
