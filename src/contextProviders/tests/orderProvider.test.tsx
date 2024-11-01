5b1f6dd46942338c4a55422f74c3286f4084783e
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-01 15:40:01 +0300

    refactor: change `renderOrderProvider` for more consice code

--- src/contextProviders/tests/orderProvider.test.tsx
+++ src/contextProviders/tests/orderProvider.test.tsx
@@ -22,9 +22,9 @@ describe('OrderProvider', () => {
     test('should provide order initial state', async () => {
       const expectedState = JSON.stringify(getOrderInitialState());
 
-      renderOrderProvider();
+      const { stateField } = renderOrderProvider();
 
-      expect(getStateFieldContent()).toBe(expectedState);
+      expect(stateField.textContent).toBe(expectedState);
     });
   });
 
@@ -32,169 +32,163 @@ describe('OrderProvider', () => {
     test('should provide order initial state', async () => {
       const expectedState = JSON.stringify(orderStateWithProducts);
 
-      renderOrderProvider(orderStateWithProducts);
+      const { stateField } = renderOrderProvider(orderStateWithProducts);
 
-      expect(getStateFieldContent()).toBe(expectedState);
+      expect(stateField.textContent).toBe(expectedState);
     });
   });
 
   describe('when scoop input value is 1', () => {
     test('should update order products correctly', async () => {
-      const user = userEvent.setup();
       const expectedState = JSON.stringify(orderStateWithScoops);
-      renderOrderProvider();
-      const input = getScoopInput();
+      const { user, stateField, scoopInput } = renderOrderProvider();
 
-      await user.type(input, '1');
+      await user.type(scoopInput, '1');
 
-      expect(getStateFieldContent()).toBe(expectedState);
+      expect(stateField.textContent).toBe(expectedState);
     });
   });
 
   describe('when topping input value is 1', () => {
     test('should update order products correctly', async () => {
-      const user = userEvent.setup();
       const expectedState = JSON.stringify(orderStateWithToppings);
-      renderOrderProvider();
-      const input = getToppingInput();
+      const { user, stateField, toppingInput } = renderOrderProvider();
 
-      await user.type(input, '1');
+      await user.type(toppingInput, '1');
 
-      expect(getStateFieldContent()).toBe(expectedState);
+      expect(stateField.textContent).toBe(expectedState);
     });
   });
 
   describe('when topping input value is 1, scoop input value is 1', () => {
     test('should update order products correctly', async () => {
-      const user = userEvent.setup();
+      const { user, stateField, toppingInput, scoopInput } = renderOrderProvider();
       const expectedState = JSON.stringify(orderStateWithProducts);
-      renderOrderProvider();
-      const toppingInput = getToppingInput();
-      const scoopInput = getScoopInput();
 
       await user.type(toppingInput, '1');
       await user.type(scoopInput, '1');
 
-      expect(getStateFieldContent()).toBe(expectedState);
+      expect(stateField.textContent).toBe(expectedState);
     });
   });
 
   describe('when input value is more than 10', () => {
     test('should update order with count 10', async () => {
-      const user = userEvent.setup();
       const expectedState = JSON.stringify(orderStateWithMaxScoops);
-      renderOrderProvider();
-      const input = getScoopInput();
+      const { user, stateField, scoopInput } = renderOrderProvider();
 
-      await user.type(input, '500');
+      await user.type(scoopInput, '500');
 
-      expect(getStateFieldContent()).toBe(expectedState);
+      expect(stateField.textContent).toBe(expectedState);
     });
   });
 
   describe('when input value is negative number', () => {
     test('should remove product from the state', async () => {
-      const user = userEvent.setup();
       const expectedState = JSON.stringify(getOrderInitialState());
-      renderOrderProvider();
-      const input = getScoopInput();
+      const { user, stateField, scoopInput } = renderOrderProvider();
 
-      input.focus();
+      scoopInput.focus();
       await user.paste('-5');
 
-      expect(getStateFieldContent()).toBe(expectedState);
+      expect(stateField.textContent).toBe(expectedState);
     });
   });
 
   describe('when input value is empty', () => {
     test('should remove product from the state', async () => {
-      const user = userEvent.setup();
       const expectedState = JSON.stringify(getOrderInitialState());
-      renderOrderProvider();
-      const input = getScoopInput();
+      const { user, stateField, scoopInput } = renderOrderProvider();
 
-      await user.type(input, '5');
-      await user.clear(input);
+      await user.type(scoopInput, '5');
+      await user.clear(scoopInput);
 
-      expect(getStateFieldContent()).toBe(expectedState);
+      expect(stateField.textContent).toBe(expectedState);
     });
   });
 
   describe('when input value is 0', () => {
     test('should remove product from the state', async () => {
-      const user = userEvent.setup();
       const expectedState = JSON.stringify(getOrderInitialState());
-      renderOrderProvider();
-      const input = getScoopInput();
+      const { user, stateField, scoopInput } = renderOrderProvider();
 
-      await user.type(input, '5');
-      await user.clear(input);
-      await user.type(input, '0');
+      await user.type(scoopInput, '5');
+      await user.clear(scoopInput);
+      await user.type(scoopInput, '0');
 
-      expect(getStateFieldContent()).toBe(expectedState);
+      expect(stateField.textContent).toBe(expectedState);
     });
   });
 
   describe('when reset button is clicked with products in order ', () => {
     test('should reset state to initial', async () => {
-      const user = userEvent.setup();
       const expectedState = JSON.stringify(getOrderInitialState());
-      renderOrderProvider(orderStateWithProducts);
+      const { user, stateField, resetOrderBtn } = renderOrderProvider(orderStateWithProducts);
 
-      await user.click(getResetOrderBtn());
+      await user.click(resetOrderBtn);
 
-      expect(getStateFieldContent()).toBe(expectedState);
+      expect(stateField.textContent).toBe(expectedState);
     });
   });
 
   describe('when create button is clicked', () => {
     test('should update order with loading status', async () => {
-      const user = userEvent.setup();
-      renderOrderProvider(orderStateWithProducts);
+      const { user, stateField, createOrderBtn } = renderOrderProvider(orderStateWithProducts);
       const expectedState = JSON.stringify(orderStateCreatingOrder);
       addNetworkInfiniteDelay(server);
 
-      await user.click(getCreateOrderBtn());
+      await user.click(createOrderBtn);
 
-      await waitFor(() => expect(getStateFieldContent()).toBe(expectedState));
+      await waitFor(() => expect(stateField.textContent).toBe(expectedState));
     });
   });
 
   describe('when create button is clicked, request is rejected', () => {
     test('should update order with error status', async () => {
-      const user = userEvent.setup();
-      renderOrderProvider(orderStateWithProducts);
+      const { user, stateField, createOrderBtn } = renderOrderProvider(orderStateWithProducts);
       const expectedState = JSON.stringify(orderStateCreatingFailed);
       addNetworkError(server);
 
-      await user.click(getCreateOrderBtn());
+      await user.click(createOrderBtn);
 
-      await waitFor(() => expect(getStateFieldContent()).toBe(expectedState));
+      await waitFor(() => expect(stateField.textContent).toBe(expectedState));
     });
   });
 
   describe('when create button is clicked, request is resolved', () => {
     test('should update order with success status', async () => {
-      const user = userEvent.setup();
-      renderOrderProvider(orderStateWithProducts);
+      const { user, stateField, createOrderBtn } = renderOrderProvider(orderStateWithProducts);
       const expectedState = JSON.stringify(orderStateCreatingSuccess);
 
-      await user.click(getCreateOrderBtn());
+      await user.click(createOrderBtn);
 
-      await waitFor(() => expect(getStateFieldContent()).toBe(expectedState));
+      await waitFor(() => expect(stateField.textContent).toBe(expectedState));
     });
   });
 });
 
-const renderOrderProvider = (initialState?: OrderContextValueType) =>
-  render(
+const renderOrderProvider = (initialState?: OrderContextValueType) => {
+  const user = userEvent.setup();
+
+  const utils = render(
     <OrderProvider initialState={initialState}>
       <OrderConsumer />
     </OrderProvider>
   );
 
-const getStateFieldContent = () => screen.getByTestId('order-state').textContent;
-const getScoopInput = () => screen.getByTestId('scoop-input');
-const getToppingInput = () => screen.getByTestId('topping-input');
-const getResetOrderBtn = () => screen.getByTestId('reset-order');
-const getCreateOrderBtn = () => screen.getByTestId('create-order');
+  const stateField = screen.getByTestId('order-state');
+  const scoopInput = screen.getByTestId('scoop-input');
+  const toppingInput = screen.getByTestId('topping-input');
+  const resetOrderBtn = screen.getByTestId('reset-order');
+  const createOrderBtn = screen.getByTestId('create-order');
+
+  return {
+    ...utils,
+    user,
+    stateField,
+    scoopInput,
+    toppingInput,
+    resetOrderBtn,
+    createOrderBtn,
+  };
+};
